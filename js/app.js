218cc126236b422a4328022d89ae5576507469e5
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-12-01 21:30:09 -0700

    added javascript to make buttons interactive

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
 //document.body.style.backgroundColor = "red";
 //test me
+const openbtn = document.querySelector('#open');
+openbtn.addEventListener('click', () => {
+    document.querySelector('.container').classList.toggle('show-nav')
+    document.querySelector('.circle-container').classList.toggle('rotate')
+})
+const closebtn = document.querySelector('#close');
+closebtn.addEventListener('click', () => {
+    document.querySelector('.container').classList.toggle('show-nav')
+    document.querySelector('.circle-container').classList.toggle('rotate')
+})
